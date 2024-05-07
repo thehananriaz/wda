bf1179099c31111d275fc60cff8eef4617600600
Hanan Riaz <thehananriaz@gmail.com>
2024-05-07 18:33:37 +0500

    weather task is done

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # wda
+Import raw data from a text file:
+
+python weather_cli.py import --file raw_weather_data.txt
+
+Perform analytics:
+
+python weather_cli.py analyze --range "2023-01-01 to 2023-01-31"
+
+Export analytics to a CSV file:
+
+python weather_cli.py export --format csv
--- weather_cli.py
+++ weather_cli.py
@@ -3,19 +3,29 @@ import sys
 import argparse
 import csv
 from datetime import datetime
+import json
 
 
-# import file cli
-# data = pd.read_csv(r"raw_weather_data.csv")
-# df = pd.DataFrame(data, columns= ["Mintemp","MaxTemp"])
-
-
-def import_data(self, file_path):
+def import_data(file_path):
     try:
         with open(file_path, "r") as file:
-            reader = csv.DictReader(file)
-            self.data = list(reader)
-        print(f"Imported data from {file_path}")
+            data = []
+            reader = csv.reader(file)
+            first = next(reader)
+            location = first[1]
+            for i in range(0, 9):
+                next(reader)
+            for row in reader:
+                tmp = float(row[1]) if row[1] else None
+                data.append(
+                    {"location": location, "date": row[0], "temperature": row[1]}
+                )
+
+            with open("data.json", "w") as json_file:
+                json.dump(data, json_file)
+
+            print(f"Imported data from {file_path}")
+            return data
     except FileNotFoundError:
         print(f"File not found: {file_path}")
 
@@ -23,41 +33,29 @@ def import_data(self, file_path):
 # export cli command
 
 
-def export_results(self, file_path, file_format="txt"):
+def export_results(file_path, file_format="txt"):
     if file_format not in ["txt"]:
         print("only support txt.")
         return
 
     try:
         with open(file_path, "w", newline="") as file:
-            if file_format == "txt":
-                file.write("Analysis Results:\n")
-                file.write(f"Average Temperature: {avg_temp:.2f}°C\n")
-                file.write(f"Minimum Temperature: {min_temp:.2f}°C\n")
-                file.write(f"Maximum Temperature: {max_temp:.2f}°C\n")
-                file.write(f"Average Humidity: {avg_humidity:.2f}%\n")
-                file.write(f"Humidity Trend: {trend_humidity}\n")
-                file.write(
-                    f"Windiest Day: {windiest_day['wind_speed']} km/h wind speed on {windiest_day['date']} \n"
-                )
-            # elif file_format == 'csv':
-            #     fieldnames = ['Metric', 'Value']
-            #     writer = csv.DictWriter(file, fieldnames=fieldnames)
-            #     writer.writeheader()
-            #     writer.writerow({'Metric': 'Average Temperature', 'Value': avg_temp})
-            #     writer.writerow({'Metric': 'Minimum Temperature', 'Value': min_temp})
-            #     writer.writerow({'Metric': 'Maximum Temperature', 'Value': max_temp})
-            #     writer.writerow({'Metric': 'Average Humidity', 'Value': avg_humidity})
-            #     writer.writerow({'Metric': 'Humidity Trend', 'Value': trend_humidity})
-            #     writer.writerow({'Metric': 'Windiest Day', 'Value': f"{windiest_day['date']} with {windiest_day['wind_speed']} km/h wind speed"})
-        # print(f"Exported analysis results to {file_path}")
+            with open("info.json", "r") as json_file:
+                data = json.load(json_file)
+
+            for key in data.keys():
+                file.write(f"{key}: {data[key]}\n")
     except IOError:
         print(f"Error exporting results to {file_path}")
 
 
 # analyz cli command
-def analyze(self, start_date, end_date):
-    if not self.data:
+def analyze(start_date, end_date):
+
+    with open("data.json", "r") as json_file:
+        data = json.load(json_file)
+
+    if not data:
         print("No data found")
         return
 
@@ -67,11 +65,11 @@ def analyze(self, start_date, end_date):
     except ValueError:
         print("Invalid date format. Please use year-month-day.")
         return
-
+    print(data)
     filtered_data = [
         entry
-        for entry in self.data
-        if start_date <= datetime.strptime(entry["date"], "%Y-%m-%d") <= end_date
+        for entry in data
+        if start_date <= datetime.strptime(entry["date"], "%Y%m%dT%H%M") <= end_date
     ]
 
     if not filtered_data:
@@ -79,65 +77,65 @@ def analyze(self, start_date, end_date):
         return
 
     temperatures = [float(entry["temperature"]) for entry in filtered_data]
-    humidity = [float(entry["humidity"]) for entry in filtered_data]
-    wind_speed = [float(entry["wind_speed"]) for entry in filtered_data]
-
+    # humidity = [float(entry["humidity"]) for entry in filtered_data]
+    # wind_speed = [float(entry["wind_speed"]) for entry in filtered_data]
     avg_temp = sum(temperatures) / len(temperatures)
     min_temp = min(temperatures)
     max_temp = max(temperatures)
-
-    avg_humidity = sum(humidity) / len(humidity)
-    trend_humidity = "increasing" if humidity[-1] > humidity[0] else "decreasing"
-
-    windiest_day = max(filtered_data, key=lambda x: float(x["wind_speed"]))
-
+    # avg_humidity = sum(humidity) / len(humidity)
+    # trend_humidity = "increasing" if humidity[-1] > humidity[0] else "decreasing"
+    # windiest_day = max(filtered_data, key=lambda x: float(x["wind_speed"]))
     print("Analysis results:")
     print(f"Average temperature: {avg_temp:.2f}°C")
     print(f"Minimum temperature: {min_temp:.2f}°C")
     print(f"Maximum temperature: {max_temp:.2f}°C")
-    print(f"Average humidity: {avg_humidity:.2f}%")
-    print(f"Humidity trend: {trend_humidity}")
-    print(
-        f"Windiest day: {windiest_day['date']} with {windiest_day['wind_speed']} km/h wind speed"
-    )
 
-
-# cmd = sys.argv[1]
-
-# if cmd == 'analyze':
-#     if sys.argv[2] == '--range':
-#         range_str = sys.argv[3].split(' ')
-#         start_date = range_str[0]
-#         end_date = range_str[2]
-#         analyze(df, start_date, end_date)
+    with open("info.json", "w") as json_file:
+        json.dump(
+            {
+                "Average temperature": round(avg_temp, 2),
+                "Minimum temperature": round(min_temp, 2),
+                "Maximum temperature": round(max_temp, 2),
+            },
+            json_file,
+        )
+    # print(f"Average humidity: {avg_humidity:.2f}%")
+    # print(f"Humidity trend: {trend_humidity}")
+    # print(
+    #     f"Windiest day: {windiest_day['date']} with {windiest_day['wind_speed']} km/h wind speed"
+    # )
 
 
 def main():
     parser = argparse.ArgumentParser(description="WEATHER CLI")
     # parser.add_argument('--import', help="Path to the input file")
+    parser.add_argument("input", help="Path to the input file")
     parser.add_argument("--file", help="Path to the input file")
     parser.add_argument("--range", help="Time range in 'YYYY-MM-DD to YYYY-MM-DD'")
     parser.add_argument("--output", help="Path to the output file")
 
     args = parser.parse_args()
 
-    analyzer = analyze()
+    print(args)
 
-    if args.command == "import":
+    if args.input == "import":
         if not args.file:
             print("Please provide a file path for import.")
         else:
-            analyzer.import_data(args.file)
+            import_data(args.file)
 
-    elif args.command == "analyze":
+    elif args.input == "analyze":
         if not args.range:
             print("Please provide a time range for analysis.")
         else:
             start_date, end_date = args.range.split(" to ")
-            analyzer.analyze(start_date, end_date)
+            analyze(start_date, end_date)
 
-    elif args.command == "export":
+    elif args.input == "export":
         if not args.output:
             print("Please provide a file path for export.")
         else:
-            analyzer.export_results(args.output, args.format)
+            export_results(args.output)
+
+
+main()
